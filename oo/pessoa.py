c182d6ddac413784e4a0a42f027dd631d76a7220
heduart <heduarte2003@yahoo.com.br>
2019-03-04 00:50:17 -0300

    Criado o método cumprimentar

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,6 +1,16 @@
-#
+# Herbert Duarte - O termo classe também pode ser utilizada como sinonimo de tipo.
 
 class Pessoa:
-    ...
+
+    def cumprimentar(self):
+        return f'Olá {id(self)}'
 
 
+if __name__ == '__main__':
+    p = Pessoa()
+    # Forma errada de chamar a classe, passando p sem necessidade
+    print(Pessoa.cumprimentar(p))
+    # Confirmando o endereço de memória criado.
+    print(id(p))
+    # p é passado implicitamente pelo python referenciando o endereço de memória.
+    print(p.cumprimentar())
