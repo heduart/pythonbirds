e8aea77ecd7b32f95e76e4423256a70004e1f028
heduart <heduarte2003@yahoo.com.br>
2019-03-04 01:31:57 -0300

    Criados atributos de instância nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,16 +1,23 @@
 # O termo classe também pode ser utilizada como sinônimo de tipo.
 
 class Pessoa:
+    def __init__(self, nome=None, idade=35):
+        self.nome = nome
+        self.idade = idade
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Luciano')
     # Forma errada de chamar a classe, passando p sem necessidade
     print(Pessoa.cumprimentar(p))
     # Confirmando o endereço de memória criado.
     print(id(p))
     # p é passado implicitamente pelo python referenciando o endereço de memória.
     print(p.cumprimentar())
+    # Atrinuindo um valor para o atributo de classe nome.
+    p.nome = 'Herbert'
+    p.idade = 39
+    print(f'\033[1;34mfazendo o teste com o atributo de classe nome: \033[1;31m{p.nome}.\033[m')
