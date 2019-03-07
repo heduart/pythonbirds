de71c478f91d8492166602d04db66e92a5d5b0c4
heduart <heduarte2003@yahoo.com.br>
2019-03-07 20:06:22 -0300

    Testes com unittest – Implementado testes com acelerar, frear e direção inicial

--- oo/test_carro.py
+++ oo/test_carro.py
@@ -1,5 +1,7 @@
 from unittest import TestCase
 from oo.carro import Motor
+from oo.carro import Direcao
+
 
 class CarroTestCase(TestCase):
     def test_velocidade_inicial(self):
@@ -7,5 +9,27 @@ class CarroTestCase(TestCase):
         # Colocar o valor esperado como primeiro em seguida o valor efetivo que será recebido.
         self.assertEqual(0, motor.velocidade)
 
+    def test_acelerar(self):
+        motor = Motor()
+        # Como estamos verificando o retorno após acelerar o veículo é de uma unidade é necessário acelerar e
+        # verificar se o retorno foi correspondido, para isso chamar .
+        motor.acelerar()
+        self.assertEqual(1, motor.velocidade)
+        motor.acelerar()
+        self.assertEqual(2, motor.velocidade)
+        motor.acelerar()
+        self.assertEqual(3, motor.velocidade)
 
+    def test_frear(self):
+        motor = Motor()
+        motor.acelerar()
+        motor.acelerar()
+        motor.acelerar()
+        motor.frear()
+        self.assertEqual(1, motor.velocidade)
+        motor.frear()
+        self.assertEqual(0, motor.velocidade)
 
+    def test_calcular_direcao(self):
+        direcao = Direcao()
+        self.assertEqual('Norte', direcao.valor)
