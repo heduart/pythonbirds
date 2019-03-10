1b511d83747a7325594e7789aac63457b6db45d5
heduart <heduarte2003@yahoo.com.br>
2019-03-10 01:21:21 -0300

    Atualizado calcular posição e posição vertical

--- atores.py
+++ atores.py
@@ -113,7 +113,8 @@ class Passaro(Ator):
         o status dos Passaro deve ser alterado para destruido, bem como o seu caracter
 
         """
-        pass
+        if self.y <= 0:
+            self.status = DESTRUIDO
 
     def calcular_posicao(self, tempo):
         """
@@ -129,7 +130,13 @@ class Passaro(Ator):
         :param tempo: tempo de jogo a ser calculada a posição
         :return: posição x, y
         """
-        return self.x, self.y
+        # if self.foi_lancado() and self.status == ATIVO:
+        # Pode ser escrito da seguinte forma.
+        if self._esta_voando():
+            delta_t = tempo - self._tempo_de_lancamento
+            self._calcular_posicao_vertical(delta_t)
+            self._calcular_posicao_horizontal(delta_t)
+        return super().calcular_posicao(tempo)
 
 
     def lancar(self, angulo, tempo_de_lancamento):
@@ -141,11 +148,30 @@ class Passaro(Ator):
         :param tempo_de_lancamento:
         :return:
         """
-        self._angulo_de_lancamento = angulo
+        self._angulo_de_lancamento = math.radians(angulo)
         self._tempo_de_lancamento = tempo_de_lancamento
+
+    def _calcular_posicao_vertical(self, delta_t):
+        angulo_radianos = self._angulo_de_lancamento
+        y_atual = self._y_inicial
+        y_atual += self.velocidade_escalar * delta_t * math.sin(angulo_radianos)
+        y_atual -= (GRAVIDADE * (delta_t ** 2)) / 2
+        self.y = y_atual
+
+    def _calcular_posicao_horizontal(self, delta_t):
+        angulo_radianos = self._angulo_de_lancamento
+        x_atual = self._x_inicial
+        x_atual += self.velocidade_escalar * delta_t * math.cos(angulo_radianos)
+        self.x = x_atual
+
+    def _esta_voando(self):
+        return self.foi_lancado() and self.status == ATIVO
+
 
 class PassaroAmarelo(Passaro):
     _caracter_ativo = 'A'
+    _caracter_destruido = 'a'
+    velocidade_escalar = 30
 
 
 class PassaroVermelho(Passaro):
